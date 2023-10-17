0a142f168ab3f10e5b221e43bbdb853d9380ca62
rayn-amii <nicolas.raymond@amii.ca>
2023-10-17 08:15:19 -0600

    Pylint coding standards applied:

--- edit_sequence.py
+++ edit_sequence.py
@@ -2,21 +2,23 @@
 Author: Nicolas Raymond
 
 Description: This file stores a program returning a list of singe-base edits
-             that increase the mRNA abundance of a gene based on its sequence.
+             that increase the rank expression of a gene based on its sequence.
 """
 from os.path import join
+from torch import load, device
+from torch.cuda import is_available
+from warnings import warn
+
+# Project imports
 from settings.paths import TRAINED_MODELS
 from src.data.modules.preprocessing import nucleotide_to_onehot
 from src.models.cnn import CNN1D
 from src.models.head import SumHead
 from src.models.plantt import PlanTT
 from src.utils.gene_editing import get_editing_strategy
-from torch import load, device
-from torch.cuda import is_available
-from warnings import warn
 
 # Batch size used to generate the embeddings of the single-base variations
-BATCH_SIZE: int = 1000 
+BATCH_SIZE: int = 1000
 
 
 def validate_sequence_format(seq: str) -> None:
@@ -32,7 +34,7 @@ def validate_sequence_format(seq: str) -> None:
     # Check if the string has exactly 1500 characters
     if len(seq) != 1500:
         raise ValueError('The sequence must contain 1500 characters')
- 
+
     # Check if each character in the string is one of the valid characters
     valid_characters = {'A', 'C', 'G', 'T', 'N', 'X'}
     for char in seq:
@@ -41,53 +43,51 @@ def validate_sequence_format(seq: str) -> None:
 
 # Execution of the program
 if __name__ == '__main__':
-    
+
     # Set the device
     if is_available():
         dev = device('cuda')
     else:
         warn('No GPU was found, expect the process to be slower.')
         dev = device('cpu')
-    
+
     # Ask for the promoter sequence
     promoter = input('\nEnter the 1500bp sequence representing the promoter region: ').upper()
     validate_sequence_format(promoter)
-    
+
     # Ask for the terminator sequence
     terminator = input('\nEnter the 1500bp sequence representing the terminator region: ').upper()
     validate_sequence_format(terminator)
-    
+
     # Ask for the budget of single-base edits
     budget = int(input('\nEnter the maximal number of single-base edits that can be applied: '))
-    
+
     # Concatenate both sequence and apply one-hot encoding
-    encoded_seq = nucleotide_to_onehot(seq=promoter+terminator).unsqueeze(dim=0)
-    
+    encoded_seq = nucleotide_to_onehot(seq=promoter+terminator).permute(1, 0)
+
     # Load PlanTT-CNN model
-    plantt = PlanTT(tower=CNN1D(seq_length=3000, feature_dropout=0, dropout=0), head=SumHead(regression=True))
+    plantt = PlanTT(tower=CNN1D(seq_length=3000, feature_dropout=0, dropout=0),
+                    head=SumHead(regression=True))
+
     plantt.load_state_dict(load(join(TRAINED_MODELS, 'planttcnn', 'planttcnn.pt')))
-    
+
     # Find the editing strategy
     edits = get_editing_strategy(plantt=plantt,
                                  seq=encoded_seq,
                                  dev=dev,
                                  max_edits=budget,
                                  batch_size=BATCH_SIZE)
-    
+
     # Print the results
     print('\nList of edits found: \n')
     total = 0
     for i, e in enumerate(edits):
         print(f'{i}. Modification: {e[0]}, Position: {e[1]}, Rank expression improvement: {e[2]}')
         total += e[2]
-    
-    print(f'\nExpected improvement in mRNA abundance: {total}\n')
-    
-    print(f'\nSee the following files to visualize the results:\n')
+
+    print(f'\nExpected improvement in rank expression: {total}\n')
+
+    print('\nSee the following files to visualize the results:\n')
     for i in range(budget):
         print(f'edit_{i}.pdf')
     print('\n')
-    
-    
-    
-    
